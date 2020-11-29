2dfdc57b3ca5cfbf19f7f258519f89a1d4b279f1
Thalles Gabriel Lima Lossurdo <thallesgabriel1307@gmail.com>
2020-11-29 13:29:32 -0300

    Web Service

--- src/services/api.js
+++ src/services/api.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export const Movies = type => { // Utilitário para este Web Service
   let movieAPI = '1ee1008f'
   return axios.create({
-    baseURL: `http://www.omdbapi.com/?apikey=${movieAPI}&${type}` // Url base que será utilizada
+    baseURL: `https://www.omdbapi.com/?apikey=${movieAPI}&${type}` // Url base que será utilizada
   });
 };
